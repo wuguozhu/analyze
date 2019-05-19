868c8d20f7b127c7e6d4a295eb7bd6504737ee10
wuguozhu <wugzhu@outlook.com>
2019-05-19 18:30:52 +0800

    feat: add example test details

--- src/main/java/com/anan/study/job/ExampleTest.java
+++ src/main/java/com/anan/study/job/ExampleTest.java
@@ -4,16 +4,19 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.hive.HiveContext;
 
 public class ExampleTest {
 
 
     public static void main(String[] args){
 
-        SparkConf sc = new SparkConf().setAppName("ExampleTest").setMaster("yarn-client");
+        SparkConf sc = new SparkConf().setAppName("ExampleTest").setMaster("local[2]");
         JavaSparkContext jsc = new JavaSparkContext(sc);
 
+        HiveContext hc = new HiveContext(jsc);
 
+        hc.sql("").show();
 
     }
 }
