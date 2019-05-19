6f978f78baf25272d93eb274709167249b693a69
wuguozhu <wuguozhu@richstonedt.com>
2019-05-19 17:54:00 +0800

    feat: add spark example test

--- src/main/java/com/anan/study/job/ExampleTest.java
+++ src/main/java/com/anan/study/job/ExampleTest.java
@@ -1,9 +1,19 @@
 package com.anan.study.job;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+
 public class ExampleTest {
 
 
     public static void main(String[] args){
+
+        SparkConf sc = new SparkConf().setAppName("ExampleTest").setMaster("yarn-client");
+        JavaSparkContext jsc = new JavaSparkContext(sc);
+
+
 
     }
 }
